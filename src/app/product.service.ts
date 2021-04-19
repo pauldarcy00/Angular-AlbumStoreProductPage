dac56a94548c35c6070cc3db15e03d10092faf0d
paul.darcy <paul.darcy@accenture.com>
2021-04-19 16:30:12 +0100

    b

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+  private _albumUrl: string = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): JSON {
+    return this._http.get(this._albumUrl)
+  }
+}
